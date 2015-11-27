036f5e7ceba6f4777fd268ea8739253d5bbf47b3
Bruce Rodrigues <brucerodrigues@github.com>
2015-11-26 22:56:40 -0200

    .

--- src/main/java/br/ufsc/configurator/api/field/ConfigRadio.java
+++ src/main/java/br/ufsc/configurator/api/field/ConfigRadio.java
@@ -1,9 +1,17 @@
 package br.ufsc.configurator.api.field;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
 public class ConfigRadio extends ConfigField {
 
-	public ConfigRadio(Integer line, Integer position, Object constant, String caption, String width,
-			boolean enabled) {
+	@Getter
+	@Setter
+	private List<Object> values;
+
+	public ConfigRadio(Integer line, Integer position, Object constant, String caption, String width, boolean enabled) {
 		super(line, position, constant, caption, width, enabled, ConfigFieldType.RADIO);
 	}
 
